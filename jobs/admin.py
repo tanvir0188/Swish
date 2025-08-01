a726978fd48ccb80b7f6cb3b0ba39ee65e64a914
tanvir0188 <arnob0188@gmail.com>
2025-08-01 16:59:50 +0600

    requirements changed

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-
-from jobs.models import Category, Job, SubCategory, JobPauseReason
-
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(SubCategory)
-admin.site.register(Job)
-admin.site.register(JobPauseReason)
+# from django.contrib import admin
+#
+# from jobs.models import Category, Job, SubCategory, JobPauseReason
+#
+# # Register your models here.
+# admin.site.register(Category)
+# admin.site.register(SubCategory)
+# admin.site.register(Job)
+# admin.site.register(JobPauseReason)
--- service_provider/admin.py
+++ service_provider/admin.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-
-from service_provider.models import CompanyProfile, Bid, TokenPackage, TokenTransaction
-
-# Register your models here.
-admin.site.register(CompanyProfile)
-admin.site.register(Bid)
-admin.site.register(TokenPackage)
-admin.site.register(TokenTransaction)
+# from django.contrib import admin
+#
+# from service_provider.models import CompanyProfile, Bid, TokenPackage, TokenTransaction
+#
+# # Register your models here.
+# admin.site.register(CompanyProfile)
+# admin.site.register(Bid)
+# admin.site.register(TokenPackage)
+# admin.site.register(TokenTransaction)

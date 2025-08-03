349deaadb84160c59194e0520a0ef6ffc56a7c3d
tanvir0188 <arnob0188@gmail.com>
2025-08-03 10:38:11 +0600

    Modified the admin panel for pre-subscription

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,7 +7,18 @@ from .models import User, PreSubscription
 class UserAdmin(admin.ModelAdmin):
     list_display = ('email', 'first_name', 'is_staff')
     search_fields = ('email', 'first_name')
-    
+class PreSubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('email', 'role', 'full_name', 'company_name', 'city', 'ice_number', 'formatted_phone_number')
+    search_fields = ('email', 'company_name')
+
+    def formatted_phone_number(self, obj):
+      if obj.phone_number:
+        return f'+{obj.phone_number}'
+      else:
+        return 'None'
+
+
+    formatted_phone_number.short_description = 'Phone Number'
 admin.site.register(User, UserAdmin)
-admin.site.register(PreSubscription)
+admin.site.register(PreSubscription, PreSubscriptionAdmin)
 
